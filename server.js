f83095b16f5e82b2e40def75f7c5ef987a4ca69c
Aserrano7 <serranoandres022@gmail.com>
2024-01-04 21:35:18 -0800

    added inquirer prompt

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const connection = require('./config/connection');
+function employeeSection() {
+    inquirer
+    .prompt({
+        name: "selected",
+        type: "list",
+        message: "Please indicate task",
+        choices: [
+            "Add Department", 
+            "Add Role", 
+            "Add Employee",
+            "View Departments", 
+            "View Roles", 
+            "View Employees",
+            "Update Employee Role",
+            "Delete Employee",
+            "Leave"
+        ]
+    })
+}
