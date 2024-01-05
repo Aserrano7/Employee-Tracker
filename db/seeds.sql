fd967c3ef67bcdd55c806b1011d9a82c847d9542
Aserrano7 <serranoandres022@gmail.com>
2024-01-04 20:52:52 -0800

    added seed.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+    department (name)
+VALUES
+    ('Engineering'),
+    ('Sales'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO
+    role (title, salary, department_id)
+VALUES
+    ('Lawyer', 400000, 1),
+    ('Salesperson', 70000, 2),
+    ('Software Engineer', 90000, 3),
+    ('Accountant', 50000, 4);
+
+INSERT INTO
+    employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Tom', 'Hanks', 1, 4),
+    ('Chris', 'Tucker', 2, 3),
+    ('Gabriel', 'Iglesias', 3, 1),
+    ('Ralph', 'Barbosa', 4, 5);
