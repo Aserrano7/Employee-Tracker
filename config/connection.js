a471ed19381d0e3d3fc59e75f828a8662416433c
Aserrano7 <serranoandres022@gmail.com>
2024-01-04 20:58:21 -0800

    added my connection

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+    host: 'localhost',
+    port: 3306,
+    database: 'user_db'
+});
+
+module.exports = connection;
